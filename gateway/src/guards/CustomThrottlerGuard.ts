75dce7db773bb881c689c85802a0afeef5d27fb3
kivaX9 <kirill.vlasov.05@inbox.tu>
2024-06-15 16:00:57 +0500

    добавил описание для CustomThrottlerGuard

--- gateway/src/guards/CustomThrottlerGuard.ts
+++ gateway/src/guards/CustomThrottlerGuard.ts
@@ -4,10 +4,18 @@ import { ThrottlerGuard } from '@nestjs/throttler'
 @Injectable()
 export class CustomThrottlerGuard extends ThrottlerGuard {
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    // Извлекаем объект запроса из контекста выполнения
     const request = context.switchToHttp().getRequest()
+
+    // Проверка на метод запроса GET (Если GET, то ThrottlerGuard применять не нужно)
     if (request.method === 'GET') {
       return true
     }
+
+    // вызывается родительский метод canActivate из класса ThrottlerGuard
     return super.canActivate(context)
   }
 }
+
+// CustomThrottlerGuard - это кастомный ThrottlerGuard,
+// который устанавливает ограничение частоты запросов
